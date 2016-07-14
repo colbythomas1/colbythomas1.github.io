8948c96b6cf54673af52c0e31ac4d3f481398d93
colbythomas1 <clthomasnj@gmail.com>
2016-07-14 14:13:08 -0400

    js

--- js/typed.js
+++ js/typed.js
@@ -328,5 +328,57 @@
         resetCallback: function() {}
     };
 
+	$( window ).resize(function() {
+$window = $(window);
+if( $window .width() > 800){
+
+ $('section[data-type="background"]').each(function(){
+ var $bgobj = $(this); // assigning the object
+
+  $(window).scroll(function() {
+
+    // Scroll the background at var speed
+    // the yPos is a negative value because we're scrolling it UP!                              
+    var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
+
+    // Put together our final background position
+    var coords = '50% '+ yPos + 'px';
+
+    // Move the background
+    $bgobj.css({ backgroundPosition: coords });
+
+ }); // window scroll Ends
+
+ });    
+}
+});
+
+
+
+$(document).ready(function(){
+$window = $(window);
+if( $window.width() > 800){
+// Cache the Window object
+
+ $('section[data-type="background"]').each(function(){
+ var $bgobj = $(this); // assigning the object
+
+  $(window).scroll(function() {
+
+    // Scroll the background at var speed
+    // the yPos is a negative value because we're scrolling it UP!                              
+    var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
+
+    // Put together our final background position
+    var coords = '50% '+ yPos + 'px';
+
+    // Move the background
+    $bgobj.css({ backgroundPosition: coords });
+
+ }); // window scroll Ends
+
+ });    
+}
+});
 
 }(window.jQuery);
